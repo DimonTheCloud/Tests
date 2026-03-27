7199f7baeb94c7f1c99478c14e389a965ec5c9c3
Dmytro Antonenko <dimchik41414@gmail.com>
2026-03-27 14:33:12 +0100

    start

---
+++ knowhow_2/knowhow_2.py
@@ -0,0 +1,7 @@
+# cd "ПУТЬ_К_ТВОЕЙ_ПАПКЕ"
+# git init
+# git add .
+# git commit -m "init"
+# git branch -M main
+# git remote add origin https://github.com/USERNAME/REPO.git
+# git push -u origin main
