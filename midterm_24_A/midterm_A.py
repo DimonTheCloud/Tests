eebf9432713d1824d97b45b6ab373f62db651c0d
Dmytro Antonenko <dimchik41414@gmail.com>
2026-03-28 21:26:46 +0100

    mid_24_A_done!

--- midterm_24_A/midterm_A.py
+++ midterm_24_A/midterm_A.py
@@ -19,14 +19,27 @@ def geneticka_predispozice_lecby(string):
         return 9
 
 def vyber_leku(string, score):
-
-    if string.count("M") >= 5 > score:
+    if string.count("M") >= 5 and score < 5:
         return "jablko"
     elif string.count("A") == 5 and string.count("M") < 2:
         return "injekce"
     else:
         return "prasky"
 
+def samovolna_mutace(string, lek):
+    if lek == "jablko":
+        return "Pokemon se uzdravil bez mutace"
 
-def samovona_mutace(string, leky):
-    pass
+    elif lek == "prasky":
+        string = string.replace("A", "M")
+        skore = string.count("M") * 3
+
+        if skore > 10:
+            return "Pokemon se vyvinul na jeho dalsi level"
+        else:
+            return "Pokemon se uzdravil, ale nema dostatecnou energii se vyvinout"
+
+    elif lek == "injekce":
+        string = string.replace("T", "M")
+        string = string.replace("Z", "M")
+        skore = string.count("M") * 3
